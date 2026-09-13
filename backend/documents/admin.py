cca69a072e82b40752ca883926708800b3a16d4d
Haitem A <haitemamirat2006@gmail.com>
2026-09-13 14:48:35 +0100

    add Document model with active_at field and register in admin; update migrations

--- backend/documents/admin.py
+++ backend/documents/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Document
 
 # Register your models here.
+
+
+
+admin.site.register(Document)
+
+
--- backend/documents/models.py
+++ backend/documents/models.py
@@ -1,8 +1,12 @@
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 user = settings.AUTH_USER_MODEL # "auth.user"
+
+
 
 
 #ORM
@@ -11,5 +15,21 @@ class Document(models.Model):
     title = models.CharField(default="Title")
     content = models.TextField(blank=True,null=True)
     active = models.BooleanField(default=True)
+    active_at = models.DateTimeField(auto_now_add=False,auto_now=False,blank=True ,null= True)
     created_at = models.DateTimeField(auto_now_add=True)#DB AUTO UPDATE THE field WHEN ITS created 
     updated_at = models.DateTimeField(auto_now=True)#db auto update this field to when its updated 
+
+
+
+    def save(self , *args, **kwargs): 
+        if(self.active and self.active_at is None):
+            self.active_at = timezone.now()
+        else:
+            self.active_at = None
+        super().save(*args, **kwargs)
+
+
+
+
+
+    
